6c62cd892d20c01d12c9fd91e7f9671a4772f4c7
ashad912 <ashad912@gmail.com>
2020-04-11 22:15:05 +0200

    chapter 2 js types

---
+++ chapter-2/sandbox.js
@@ -0,0 +1,19 @@
+let age = 25;
+const year = 2019;
+var points = 100;
+
+console.log(age, year, points)
+
+//comment
+/* block
+    comment */
+
+/* JS Data Types
+    Number
+    String
+    Boolean
+    Null
+    Undefined
+    Object - Literals etc.
+    Symbols - unieque identifiers
+*/
