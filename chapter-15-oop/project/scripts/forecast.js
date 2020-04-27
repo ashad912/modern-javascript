be0518bfbcf1f355ba41737cf5b466ab5cad01c7
ashad912 <ashad912@gmail.com>
2020-04-27 11:21:09 +0200

    lesson 12x unstable

--- chapter-15-oop/project/scripts/forecast.js
+++ chapter-15-oop/project/scripts/forecast.js
@@ -1,3 +1,29 @@
+class Forecast {
+    constructor(){
+        this.key = 'wQZQJ5KIONkAezKp10307rmJGc2mlKrz'
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/'
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    }
+
+    getCity = async(city) => {
+        const query = `?apikey=${this.key}&q=${city}`
+
+        const res = await fetch(this.cityURI + query)
+        const data = await res.json()
+    
+        return data[0]
+    }
+
+    getWeather = async(id) => {
+        const query = `${id}?apikey=${this.key}`
+
+        const res = await fetch(this.weatherURI + query)
+        const data = await res.json()
+
+        return data[0]
+    }
+}
+
 const key = 'wQZQJ5KIONkAezKp10307rmJGc2mlKrz'
 
 const getCity = async (city) => {
